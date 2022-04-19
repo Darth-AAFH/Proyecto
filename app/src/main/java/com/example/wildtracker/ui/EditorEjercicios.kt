fb404034e479cf87d1077bd8458984b0f955b249
SantiAsaf <santiasaf@hotmail.com>
2022-04-19 17:43:50 -0500

    Correcciones Plantillas

--- app/src/main/java/com/example/wildtracker/ui/EditorEjercicios.kt
+++ app/src/main/java/com/example/wildtracker/ui/EditorEjercicios.kt
@@ -1,7 +1,6 @@
 package com.example.wildtracker.ui
 
 import android.annotation.SuppressLint
-import android.content.Intent
 import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
 import android.view.MenuItem
