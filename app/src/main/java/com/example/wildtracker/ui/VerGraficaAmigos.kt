8c8ebde0d850929ba1b308d0c2cea78e093c8837
Darth_AAFH <aalexhev@gmail.com>
2022-11-30 02:00:59 -0600

    Se edita visualmente el apartado de graficas

--- app/src/main/java/com/example/wildtracker/ui/VerGraficaAmigos.kt
+++ app/src/main/java/com/example/wildtracker/ui/VerGraficaAmigos.kt
@@ -1,9 +1,22 @@
 package com.example.wildtracker.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.MenuItem
+import android.view.View
 import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import com.example.wildtracker.LoginActivity
 import com.example.wildtracker.R
+import com.example.wildtracker.musica.mPlayerActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.material.navigation.NavigationView
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_ver_grafica_amigos.*
 import org.achartengine.ChartFactory
 import org.achartengine.GraphicalView
@@ -12,7 +25,7 @@ import org.achartengine.model.XYSeries
 import org.achartengine.renderer.XYMultipleSeriesRenderer
 import org.achartengine.renderer.XYSeriesRenderer
 
-class VerGraficaAmigos : AppCompatActivity() {
+class VerGraficaAmigos : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private var grafica: GraphicalView? = null
     private var datosGrafica: XYSeries? = null
@@ -28,6 +41,7 @@ class VerGraficaAmigos : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ver_grafica_amigos)
+        initNavigationView()
 
         val b = intent.extras //b toma el id de la rutina a trabajar
         if (b != null) {
@@ -71,6 +85,20 @@ class VerGraficaAmigos : AppCompatActivity() {
             textViewDia7!!.setText("L"); textViewDia1!!.setText("M"); textViewDia2!!.setText("M"); textViewDia3!!.setText("J")
             textViewDia4!!.setText("V"); textViewDia5!!.setText("S"); textViewDia6!!.setText("D")
         }
+
+        val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar_main)
+        toolbar.title = "Grafica Semanal de $nombre"
+        setSupportActionBar(toolbar)
+
+        com.example.wildtracker.ui.drawer = findViewById(R.id.drawerlayout)!!
+        val toggle = ActionBarDrawerToggle(
+            this, drawer, toolbar, R.string.bar_title,
+            R.string.navigation_drawer_close
+        )
+        com.example.wildtracker.ui.drawer.addDrawerListener(toggle)
+        toggle.syncState()
+
+
     }
 
     private fun iniciarGrafica() {
@@ -98,5 +126,124 @@ class VerGraficaAmigos : AppCompatActivity() {
 
         seriesRendererGrafica = XYSeriesRenderer()
         renderGrafica.addSeriesRenderer(seriesRendererGrafica)
+    }
+    private fun initNavigationView() {
+
+        val navigationView: NavigationView = findViewById(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
+
+        val headerView: View = LayoutInflater.from(this)
+            .inflate(R.layout.nav_header_main, navigationView, false)
+        //Header para datos del usuario
+        navigationView.removeHeaderView(headerView)
+        //para actualizar los datos del header
+        navigationView.addHeaderView(headerView)
+
+        val tvUser: TextView = headerView.findViewById(R.id.tvUser)
+        tvUser.text = MainActivity.user
+
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_perfil -> callPerfilActivity()
+            R.id.nav_inicio -> callInicioActivity()
+            R.id.nav_plantillas -> callPlantillasActivity()
+            R.id.nav_ejercicio -> callEjercicioActivity()
+            R.id.nav_maps -> callMapsActivity()
+            R.id.nav_metas -> callMetasActivity()
+            R.id.nav_ranking -> callRankingActivity()
+            R.id.nav_chat -> callChatActivity()
+            R.id.logOut -> signOut()
+            R.id.nav_musica ->callMusica()
+            R.id.nav_amigos ->callAmigosActivity()
+            R.id.Settings->callAjustesActivity()
+            R.id.nav_seguimiento->callSeguimientoActivity()
+        }
+
+        com.example.wildtracker.ui.drawer.closeDrawer(GravityCompat.START) // cerrar menu
+
+        return true
+    }
+    private fun callRankingActivity() {
+        val intent = Intent(this, RankingActivity::class.java)
+        startActivity(intent)
+    }
+    private fun callAjustesActivity() {
+        val intent = Intent(this, SettingsActivity::class.java)
+        startActivity(intent)
+    }
+    private fun callAmigosActivity() {
+        val intent = Intent(this, Activity_Amigos::class.java)
+        startActivity(intent)
+    }
+    private fun callMusica() {
+        val intent = Intent(this, mPlayerActivity::class.java)
+        startActivity(intent)
+    }
+    private fun callPerfilActivity() {
+        val intent = Intent(this, PerfilActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun callInicioActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun callPlantillasActivity() {
+        val intent = Intent(this, PlantillasActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun callEjercicioActivity() {
+        val intent = Intent(this, EjercicioActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun callMapsActivity() {
+        val intent = Intent(this, MapsActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun callSeguimientoActivity() {
+        val intent = Intent(this, SeguimientoActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun callChatActivity() {
+        val intent = Intent(this, ChatActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun callMetasActivity() {
+        val intent = Intent(this, MetasActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun signOut() {
+
+        LoginActivity.useremail = ""
+        FirebaseAuth.getInstance().signOut()
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("727481893022-adct709pnvj5tlihh532i6gjgm26thh6.apps.googleusercontent.com")
+            .requestEmail()
+            .build()
+
+        val googleSignInClient = GoogleSignIn.getClient(this, gso)
+        googleSignInClient.signOut()
+        //Cierra sesion y manda devuelta al login
+        deleteAppData()
+    }
+    private fun deleteAppData() {
+        try {
+            // clearing app data
+            val packageName = applicationContext.packageName
+            val runtime = Runtime.getRuntime()
+            runtime.exec("pm clear $packageName")
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 }
